f49b878a3c3e8d3a96c2d9785f8692333e0792df
Vasyl <admin@gmail.com>
2019-02-20 18:22:36 +0200

    ajax function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-alert ('ok');
+function exchange(){
+var xhr=new XMLHttpReguest();
 
+//подготовка обработки успеха или неудачи запроса
+xhr.onreadystatechange=function(){
+    if(xhr.readyState===4){
+	if(xhr.status===200){
+	    //действие в случае успеха
+	    alert('запрос выполнен');
+	}else{
+	    //TODO можно выполнить обработку не 200 статуса
+	    alert('возникла проблема');
+	}
+    }
+};
 
+//подготовка запроса
+xhr.open('GET', 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+
+//отправка запроса
+xhr.send();
+}
