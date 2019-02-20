06578c51eb3d86c69faefb5bd1dcc8ddf4c0063d
Vasyl <admin@gmail.com>
2019-02-20 20:05:07 +0200

    выводим таблицу с курсами валют

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-function exchange(){
-var xhr=new XMLHttpReguest();
-
+function exchange() {
+var xhr = new XMLHttpRequest();
 //подготовка обработки успеха или неудачи запроса
-xhr.onreadystatechange=function(){
-    if(xhr.readyState===4){
-	if(xhr.status===200){
-	    //действие в случае успеха
-	    alert('запрос выполнен');
-	}else{
-	    //TODO можно выполнить обработку не 200 статуса
-	    alert('возникла проблема');
-	}
-    }
+	xhr.onreadystatechange = function () {
+	if (xhr.readyState === 4) {
+	if (xhr.status === 200) {
+	//действие в случае успеха
+	//alert('запрос выполнен');
+	var json_exchange = xhr.responseText;
+		//преобразование JSON в объукт по старинке
+		//var exchange_rates = eval(json_exchange);
+		var exchange_rates = JSON.parse(json_exchange);
+		console.log(exchange_rates);
+		//TODO вывод всех курсов в таблицу
+		
+} else {
+		//TODO можно выполнить обработку не 200 статуса
+		alert('возникла проблема');
+}
+}
 };
 
 //подготовка запроса
@@ -20,3 +26,21 @@ xhr.open('GET', 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=
 //отправка запроса
 xhr.send();
 }
+
+function init_exchange_module(element_id) {
+		//поиск тэга для вывода в него
+		var el = document.getElementById(element_id);
+		//el.innerHTML ='<input type="button" value="получить курс"/>';
+		var btn = document.createElement('input');
+		btn.type = 'button';
+		btn.value = 'получить курс';
+		el.appendChild(btn);
+		//обработчик события нажатия кнопки
+		btn.onclick = function () {
+		exchange();
+};
+}
+
+window.onload = function () {
+			init_exchange_module('exchange');
+};
