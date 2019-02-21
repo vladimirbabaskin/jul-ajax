b4677d8fcf454d9ad27725ff32e4c5d508a95996
vladimirbabaskin <vladimirbabaskin@gmail.com>
2019-02-21 13:04:44 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 function exchange() {
-var xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest(); //создание AJAX запроса при помощи объекта  XMLHttpRequest()
 //подготовка обработки успеха или неудачи запроса
 	xhr.onreadystatechange = function () {
 	if (xhr.readyState === 4) {
-	if (xhr.status === 200) {
-	//действие в случае успеха
-	//alert('запрос выполнен');
-	var json_exchange = xhr.responseText;
+		if (xhr.status === 200) {
+		//действие в случае успеха
+		//alert('запрос выполнен');
+		var json_exchange = xhr.responseText;
 		//преобразование JSON в объукт по старинке
 		//var exchange_rates = eval(json_exchange);
 		var exchange_rates = JSON.parse(json_exchange);
 		console.log(exchange_rates);
 		//TODO вывод всех курсов в таблицу
-		
+		console.log([]);
 } else {
-		//TODO можно выполнить обработку не 200 статуса
+		//TODO можно выполнить обработку не 200-го статуса
 		alert('возникла проблема');
 }
 }
 };
 
-//подготовка запроса
+//подготовка запроса метод "open"
 xhr.open('GET', 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
 
-//отправка запроса
+//отправка запроса метод "send"
 xhr.send();
 }
 
